80465ea2f9449347781864634da8778cf81e4c05
Julio Rodrigo <127313333+JRodrigo19@users.noreply.github.com>
2025-08-13 14:05:33 -0300

    feat: criar esqueleto inicial do script com menu e variável de opção
    - Adiciona título e menu inicial usando echo
    - Cria variável `opcao` para armazenar a escolha do usuário
    - Estrutura pronta para receber implementações futuras

---
+++ SysCare.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+echo ========================================
+echo      FERRAMENTA PARA USO TECNICO
+echo                SYSCARE
+echo ========================================
+
+echo.
+echo [1] Verificacao de arquivos do windows (SFC)
+echo [0] Exit
+
+echo.
+set /p opcao=ESCOLHA UMA OPCAO:
+echo.
+pause
